53b875a52538948284f55e0d937747288ab40906
pacpac17@gmail.com <pacpac17@gmail.com>
2026-05-02 12:00:47 -0500

    vainas de Cache

--- WebApi/obj/Debug/net10.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net10.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ec5e4269da3a74652963f6f078ab1e28c5c239f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
